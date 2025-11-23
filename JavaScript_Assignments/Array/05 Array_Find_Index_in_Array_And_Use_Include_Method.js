53a5c8118f673ad5b35b8aea8984b647303a8982
Guruprasad_Jadhav <guruprasadjadhav911@gmail.com>
2025-11-23 10:58:40 +0530

    Array Find Index in Array And Use Include Method

---
+++ JavaScript_Assignments/Array/05 Array_Find_Index_in_Array_And_Use_Include_Method.js
@@ -0,0 +1,10 @@
+let Name = ["Fork","Infosystem","Karad",10,false];
+
+console.log(Name);
+
+console.log(Name);
+
+console.log(Name.indexOf("Karad"));
+
+console.log(Name.includes(10));
+console.log(Name.includes(5));
