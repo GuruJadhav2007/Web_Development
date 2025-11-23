b8f0ce5e230d80916741d6227699441a9ac2a05c
Guruprasad_Jadhav <guruprasadjadhav911@gmail.com>
2025-11-23 11:02:43 +0530

    Array Slice Method

---
+++ JavaScript_Assignments/Array/06 Array_Slice_Method.js
@@ -0,0 +1,10 @@
+let Fruits = ["Banana","Apple","Mango","Orange","Coconut"];
+
+console.log(Fruits);
+
+//let slicedFruits = Fruits.slice(2,4);
+
+//console.log(slicedFruits);
+let splicedArray = Fruits.splice(2,2,"Rassberry");
+
+console.log(splicedArray);
