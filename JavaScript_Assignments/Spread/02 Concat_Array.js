b83a87cad547e223a826d2aea6c02508f503e5e1
Guruprasad Jadhav <guruprasadjadhav911@gmail.com>
2026-06-08 14:54:02 +0530

    Concat_Array

---
+++ JavaScript_Assignments/Spread/02 Concat_Array.js
@@ -0,0 +1,12 @@
+
+// Used in Array
+
+
+let Fruits = ["Apple","Banana","Mango"];
+let MoreFruits = ["PineApple","Lichi","Papaya"];
+
+let All = [...Fruits,...MoreFruits];
+
+console.log(Fruits);
+console.log(MoreFruits);
+console.log(All);
